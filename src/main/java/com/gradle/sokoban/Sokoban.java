52af4e7eb181e0448640cd4a071099de270339e5
Diego-Valladares <diego14vb@gmail.com>
2022-02-09 17:34:15 -0600

    Creacion de Proyecto

---
+++ src/main/java/com/gradle/sokoban/Sokoban.java
@@ -0,0 +1,22 @@
+package com.gradle.sokoban;
+import java.util.*;
+import java.io.*;
+
+
+public class Sokoban
+{
+    private Scanner input = null;
+
+    public static void main (String[] args){
+
+        Sokoban sokoban = new Sokoban();
+        sokoban.run();
+    }
+
+    public void run(){
+
+        System.out.println("test");
+    }
+
+
+}
