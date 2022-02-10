6cfa4f8122cd167a4853a19a82d7dbc4a6e2c2e4
SebasJC <sebasjica@gmail.com>
2022-02-09 18:53:19 -0600

    Actualizar método run
    Hice que se le leyeran datos.

--- src/main/java/com/gradle/sokoban/Sokoban.java
+++ src/main/java/com/gradle/sokoban/Sokoban.java
@@ -5,20 +5,37 @@ import java.io.*;
 
 public class Sokoban
 {
-    private Scanner input = null;
+ private Scanner input = null;
 
-    public static void main (String[] args){
+    public static void main (String[] args) {
 
         Sokoban sokoban = new Sokoban();
         sokoban.run();
     }
 
-    public void run(){
+    public void run() {
+      input = new Scanner(System.in);
+      this.input.useDelimiter("[\\s]*");
+      int filas = this.input.nextInt();
+      int columnas = this.input.nextInt();
+      int cajas = this.input.nextInt();
+      char[][] tablero = new char[filas][columnas];
+      for (int indexFila = 0; indexFila < filas; indexFila++) {
+        for (int indexColumna = 0; indexColumna < columnas; indexColumna++) {
+          tablero[indexFila][indexColumna] = this.input.next().charAt(0);
+        }
+      }
 
-        System.out.println("test");
+      posicionCajas(filas, columnas, cajas, tablero);
+      input.close();
     }
 
+    public void posicionCajas(int filas, int columnas, int cajas,
+                              char[][] tablero) {
 
-
-
+    }
 }
+
+
+
+
