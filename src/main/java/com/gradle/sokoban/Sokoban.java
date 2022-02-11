ae03b46fae4c7cb237ef63cd6846dea43d3f83d8
Diego-Valladares <diego14vb@gmail.com>
2022-02-10 23:32:18 -0600

    Se agregaron 2 nuevos metodos, movimientosValidos y posicionCajasBloq Se logro hacer que el programa lea el .txt del caso de prueba. Se imprime un 3 de los 4 estados del tablero. Tambien se agrego gran cantidad de documentaciones javadoc al programa.

--- src/main/java/com/gradle/sokoban/Sokoban.java
+++ src/main/java/com/gradle/sokoban/Sokoban.java
@@ -3,49 +3,242 @@ import java.util.*;
 import java.io.*;
 import java.lang.*;
 
+/**
+ * Clase que indica si existe o no victoria, las posiciones de las cajas, ya
+ * sea resueltas o no, así como si están bloqueadas; también se indica si es
+ * posible que el jugador se mueva hacia cada uno de los puntos cardinales y la
+ * posición en ese punto cardinal hacia la que se puede mover, según las reglas
+ * de Sokoban.
+ */
 
-public class Sokoban
-{
- private Scanner input = null;
+public class Sokoban {
 
-    public static void main (String[] args) {
+    /**
+     * Método que arranca el programa.
+     *
+     * @param args argumentos del programa.
+     */
+
+    public static void main(String[] args) {
+
+        /**
+        * Creación de nueva instancia de la clase Sokoban.
+        */
 
         Sokoban sokoban = new Sokoban();
+
+        /**
+         * Llamado del método "run" para iniciar con la funcionalidad del
+         * programa.
+         */
+
         sokoban.run();
+
     }
+
+    /**
+     * Ejecuta la logica del programa; en el se busca un archivo de texto en la direccion proporcionada,
+     * si no lo encuentra imprimira un mensaje de error, de lo contrario el programa leera el contenido del
+     * archivo y con el creeara el tablero de juego, luego llamara a los metodos necesarios para resolver
+     * el problema.
+     */
 
     public void run() {
-      input = new Scanner(System.in);
-      this.input.useDelimiter("[\\s]*");
-      int filas = this.input.nextInt();
-      int columnas = this.input.nextInt();
-      int cajas = this.input.nextInt();
-      String[][] tablero = new String[filas][columnas];
-      for (int indexFila = 0; indexFila < filas; indexFila++) {
-        for (int indexColumna = 0; indexColumna < columnas; indexColumna++) {
-          tablero[indexFila][indexColumna] = this.input.next();
-        }
-      }
 
-      posicionCajas(filas, columnas, cajas, tablero);
-      input.close();
+        /**
+         * Variable que indica el número de filas que tiene el tablero.
+         */
+
+        int filas = 0;
+
+        /**
+         * Variable que indica el número de columnas que tiene el tablero.
+         */
+
+        int columnas = 0;
+
+        /**
+         * Variable que indica el número de cajas que hay en el tablero.
+         */
+
+        int cajas = 0;
+
+        /**
+         * Variable estilo matriz que contiene los datos del tablero actual.
+         */
+
+        String[][] tablero = new String[0][];
+
+        /**
+         * El programa intenta leer el archivo .txt en la direccion proporcionada.
+         * Si el programa detecta y lee los datos con exito, continua.
+         */
+
+        try {
+            /**
+             * NOTA: La direccion de los casos de prueba del programa muy
+             *       probablemente sea diferente a la de sucomputadora.
+             *       Para arreglar esto, simplemente consiga la direccion del folder "Sokoban",
+             *       por ejemplo: "DISK:\\Users\\USER\\Desktop\\Sokoban\\tests\\input001.txt"
+             *       y reemplaze el contenido de "new File ("LO QUE ESTA AQUI")"
+             *       con su direccion del caso de prueba.
+             */
+
+
+            /**
+             * Crea un archivo interno con el contenido del caso de prueba.
+             */
+
+            File doc = new File("C:\\Users\\diego\\Desktop\\Things\\Educacion\\UCR\\Verano_Year1\\Sokoban\\tests\\input000.txt");
+
+
+            /**
+             * Lee los datos del archivo interno.
+             */
+
+            Scanner input = new Scanner(doc);
+
+            /**
+             * Se toman los valores "int" al principio del archivo.
+             */
+
+            try {
+                filas = input.nextInt();
+                columnas = input.nextInt();
+                cajas = input.nextInt();
+
+            }
+
+            /**
+             * Si los valores no son int, imprime un mensaje de error y detiene el programa.
+             */
+
+            catch(InputMismatchException e) {
+                System.out.println("Entrada invalida.");
+                e.printStackTrace();
+                System.exit(0);
+            }
+
+            /**
+             * Tambien si los valores son 0 o negativos imprime un mensaje de error y cierra el programa.
+             */
+
+            if (filas <= 0 || columnas <= 0 || cajas <= 0){
+                System.out.println("Entrada invalida.");
+                System.exit(0);
+            }
+
+            /**
+             * Delimitador para ignorar espacios en blanco o vacios.
+             */
+
+            input.useDelimiter("[\\s]*");
+
+            /**
+             * Se crea en tablero con las dimensiones validas.
+             */
+
+            tablero = new String[filas][columnas];
+
+            /**
+             * Ciclo para agregar los datos al tablero.
+             */
+
+            for (int row = 0; row < filas; row++) {
+                for (int col = 0; col < columnas; col++) {
+                    tablero[row][col] = input.next();
+                }
+            }
+
+            /**
+             * Ciclo para imprimir el tablero, asi el usuario podra visualizarlo.
+             */
+            System.out.printf("%10s %n","Tablero");
+
+            for (int row = 0; row < filas; row++) {
+                System.out.println("");
+                for (int col = 0; col < columnas; col++) {
+                    System.out.printf("%3s",tablero[row][col]);
+                }
+            }
+
+            System.out.println("");
+
+            /**
+             * Cierra el input debido a que ya no se necesita.
+             */
+            input.close();
+        }
+
+        /**
+         * Si el programa no detecta un archivo .txt valido imprime un mensaje de error.
+         */
+
+        catch (FileNotFoundException err) {
+            System.out.println("Input file not found.");
+            err.printStackTrace();
+        }
+
+
+        System.out.println("");
+        posicionCajas(filas, columnas, cajas, tablero);
+        System.out.println("");
+        posicionCajasBloq(filas, columnas, cajas, tablero);
+        System.out.println("");
+        movimientosValidos(filas, columnas, cajas, tablero);
+
     }
 
-
-
-
+    /**
+     * Método donde se recorre la matriz para encontrar las posiciones en las que
+     * están las cajas y guarda sus posiciones, estén resueltas o no.
+     *
+     * @param filas Recibe un parámetro de tipo {@code int} que indica el número
+     *              de filas de la matriz donde se almacenan los caracteres del
+     *              tablero.
+     * @param columnas Recibe un parámetro de tipo {@code int} que indica el
+     *                 número de filas de la matriz donde se almacenan los
+     *                 caracteres del tablero.
+     * @param cajas Recibe un parámetro de tipo {@code int} que indica el número
+     *              de cajas que hay en el tablero.
+     * @param tablero Recibe una matriz de tipo {@code String[][]} que contiene
+     *                los caracteres del tablero.
+     */
 
     public void posicionCajas(int filas, int columnas, int cajas,
                               String[][] tablero) {
+        /**
+         * Variable de tipo arreglo en donde se guardan las coordenadas de las cajas.
+         */
+
       String[] coordenadasCajas = new String[cajas];
+        /**
+         * Variable que sirve de contador.
+         */
       int contadorCoordenadas = 0;
+
+        /**
+         * Ciclo en donde se recorre el tablero en busqueda de cajas.
+         */
+
       for (int indexFila = 0; indexFila < filas; indexFila++) {
         for (int indexColumna = 0; indexColumna < columnas; indexColumna++) {
+            /**
+             * Si se encuentra a la "X" que es una posición de una caja ya resuelta,
+             * la coordenada se guarda en "coordenadasCajas" con un "*" al final
+             * y se aumenta por 1 la variable "contadorCoordenadas".
+             */
           if (tablero[indexFila][indexColumna].equals("X")) {
             coordenadasCajas[contadorCoordenadas] =
                 String.format("r0%dc0%d* " ,indexFila, indexColumna);
             contadorCoordenadas++;
-          } else if (tablero[indexFila][indexColumna].equals("*")) {
+          }
+          /**
+           * Si se encuentra a la "*" que es una posición de una caja sin resolver,
+           * la coordenada se guarda en "coordenadasCajas" sin ningun caracter extra
+           * y se aumenta por 1 la variable "contadorCoordenadas".
+           */
+          else if (tablero[indexFila][indexColumna].equals("*")) {
             coordenadasCajas[contadorCoordenadas] =
                 String.format("r0%dc0%d " ,indexFila, indexColumna);
             contadorCoordenadas++;
@@ -53,7 +246,177 @@ public class Sokoban
         }
       }
 
+        /**
+         * Ciclo para imprimir las coordenadas de las cajas.
+         */
+
+        System.out.printf("%19s: ", "Cajas");
+        for (int i = 0; i < coordenadasCajas.length; i++){
+          System.out.print(coordenadasCajas[i]);
+      }
+
     }
+
+    /**
+     * Metodo en donde se recorre el tablero para encontrar las cajas bloqueadas y
+     * luego guardarlas en un arreglo, sin contar cajas ya resueltas.
+     *
+     * @param filas Recibe un parámetro de tipo {@code int} que indica el número
+     *              de filas de la matriz donde se almacenan los caracteres del
+     *              tablero.
+     * @param columnas Recibe un parámetro de tipo {@code int} que indica el
+     *                 número de filas de la matriz donde se almacenan los
+     *                 caracteres del tablero.
+     * @param cajas Recibe un parámetro de tipo {@code int} que indica el número
+     *              de cajas que hay en el tablero.
+     * @param tablero Recibe una matriz de tipo {@code String[][]} que contiene
+     *                los caracteres del tablero.
+     */
+
+    public void posicionCajasBloq(int filas, int columnas, int cajas, String[][] tablero){
+
+        /**
+         * Ciclo para determinar la cantidad de cajas bloqueadas en el tablero.
+         */
+
+        int cajasBloq = 0;
+        for (int row = 0; row < filas; row++){
+            for (int col = 0; col < columnas; col++){
+                if (tablero[row][col].equals("*")){
+                    cajasBloq++;
+                }
+            }
+        }
+
+        /**
+         * Arreglo para almacenar las coordenadas de las cajas bloqueadas.
+         */
+        String [] coordenadaCajasBloq = new String [cajasBloq];
+
+        /**
+         * Variable de estilo contador.
+         */
+
+        int contadorCajasBloq = 0;
+
+        /**
+         * Ciclo para recorrer el tablero en busqueda de cajas bloqueadas.
+         */
+        for (int row = 0; row < filas; row++){
+            for (int col = 0; col < columnas; col++){
+                /**
+                 * Si la coordenada actual es una caja sin resolver continua.
+                 */
+                if (tablero[row][col].equals("*")){
+                    /**
+                     * Si la coordenada actual tiene una pared ARRIBA y una pared DERECHA o IZQUIERDA
+                     * Significa que esta bloqueada, por lo que se agrega la coordenada al
+                     * arreglo que contiene las coordenadas y se le suma 1 al contador.
+                     */
+                    if (tablero[row-1][col].equals("#")
+                            && (tablero[row][col+1].equals("#")
+                            || tablero[row][col-1].equals("#"))){
+                        coordenadaCajasBloq[contadorCajasBloq] =
+                                String.format("r0%dc0%d ",row, col);
+                        contadorCajasBloq++;
+                    }
+                    /**
+                     * Si la coordenada actual tiene una pared ABAJO y una pared DERECHA o IZQUIERDA
+                     * Significa que esta bloqueada, por lo que se agrega la coordenada al
+                     * arreglo que contiene las coordenadas y se le suma 1 al contador.
+                     */
+                    else if (tablero[row+1][col].equals("#")
+                            && (tablero[row][col+1].equals("#")
+                            || tablero[row][col-1].equals("#"))) {
+                        coordenadaCajasBloq[contadorCajasBloq] =
+                                String.format("r0%dc0%d ", row, col);
+                        contadorCajasBloq++;
+                    }
+                }
+            }
+        }
+
+        /**
+         * Ciclo para imprimir las coordenadas de las bloqueadas.
+         */
+
+        System.out.printf("%19s: ", "Cajas Bloqueadas");
+        for (int i = 0; i < coordenadaCajasBloq.length; i++) {
+            System.out.print(coordenadaCajasBloq[i]);
+        }
+
+    }
+
+    /**
+     * Metodo donde se recorre la matriz para encontrar la posicion del jugador, y luego
+     * fijarse en cuales movimientos (NORTE, ESTE, SUR, OESTE; en ese orden) son validos.
+     *
+     * @param filas Recibe un parámetro de tipo {@code int} que indica el número
+     *              de filas de la matriz donde se almacenan los caracteres del
+     *              tablero.
+     * @param columnas Recibe un parámetro de tipo {@code int} que indica el
+     *                 número de filas de la matriz donde se almacenan los
+     *                 caracteres del tablero.
+     * @param cajas Recibe un parámetro de tipo {@code int} que indica el número
+     *              de cajas que hay en el tablero.
+     * @param tablero Recibe una matriz de tipo {@code String[][]} que contiene
+     *                los caracteres del tablero.
+     */
+
+    public void movimientosValidos (int filas, int columnas, int cajas, String[][] tablero){
+
+        String [] movimientosVal = new String[4]; // Hay cuatro direcciones que tomar en cuenta nada mas.
+
+        for (int row = 0; row < filas; row++){  //Orden de direccion: ARRIBA DERECHA ABAJO IZQUIERDA
+            for (int col = 0; col < columnas; col++){
+                if (tablero[row][col].equals("@")){
+
+                    if (tablero[row-1][col].equals(".")
+                            || tablero[row-1][col].equals("O")
+                            || tablero[row-1][col].equals("X")){
+                        movimientosVal[0] = String.format("r0%dc0%d ",row-1, col);
+                    }
+                    else{
+                        movimientosVal[0] = "-";
+                    }
+
+                    if (tablero[row][col+1].equals(".")
+                            || tablero[row][col+1].equals("O")
+                            || tablero[row][col+1].equals("X")){
+                        movimientosVal[1] = String.format("r0%dc0%d ",row, col+1);
+                    }
+                    else{
+                        movimientosVal[1] = "-";
+                    }
+
+                    if (tablero[row+1][col].equals(".")
+                            || tablero[row+1][col].equals("O")
+                            || tablero[row+1][col].equals("X")){
+                        movimientosVal[2] = String.format("r0%dc0%d ",row+1, col);
+                    }
+                    else{
+                        movimientosVal[2] = "-";
+                    }
+
+                    if (tablero[row][col-1].equals(".")
+                            || tablero[row][col-1].equals("O")
+                            || tablero[row][col-1].equals("X")){
+                        movimientosVal[3] = String.format("r0%dc0%d ",row, col-1);
+                    }
+                    else{
+                        movimientosVal[3] = "-";
+                    }
+
+                }
+            }
+        }
+
+        System.out.printf("Movimientos Validos: N:%s E:%s S:%s O:%s",movimientosVal[0],movimientosVal[1],
+                                                                    movimientosVal[2], movimientosVal[3]);
+    }
+
+
+
 }
 
 
